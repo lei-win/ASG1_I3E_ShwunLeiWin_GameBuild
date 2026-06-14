0c2e5feba65293d75b77113ce6faabed868cc4c7
lei-win <s10272765@connect.np.edu.sg>
2026-06-14 12:15:15 +0800

    setup proj environment and script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,64 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Keeping track of stats
+    public int currentPatience = 5;
+    public int itemsCollected = 0;
+    public int targetItems = 15;
+
+    // Check if the player has visited the register after collecting 15 items
+    private bool canWin = false;
+
     void Start()
     {
-        print("hello world");
+        Debug.Log("Shift Started! Collect 15 items and avoid customers.");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Called by items when the player picks them up
+    public void CollectItem()
     {
-        
+        itemsCollected++;
+        Debug.Log("Items Collected: " + itemsCollected + "/" + targetItems);
+
+        if (itemsCollected >= targetItems)
+        {
+            canWin = true;
+            Debug.Log("All items collected! Head back to the Cashier Counter to finish your shift!");
+        }
+    }
+
+    // Called by customers when the player enters their vicinity
+    public void LosePatience(int damage)
+    {
+        currentPatience -= damage;
+        Debug.Log("Patience remaining: " + currentPatience);
+
+        if (currentPatience <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    // Called when the player interacts with the Cashier Counter
+    public void CheckWinCondition()
+    {
+        if (canWin)
+        {
+            Debug.Log("YOU WIN! Shift complete, clocking out!");
+            // Later we can hook this up to a win screen popup!
+        }
+        else
+        {
+            Debug.Log("You haven't collected 15 items yet! Keep cleaning!");
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("GAME OVER! You lost your patience and lost your job!");
+        // Later we can hook this up to a game over popup!
+        Time.timeScale = 0f; // Freezes the game logic
     }
 }
